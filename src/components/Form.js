6d45ecb8e1cf6d351fd282e31091e40bb792b89f
Whitney Muchira <whitneymuchira@gmail.com>
2022-07-19 09:02:56 +0300

    Done

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,16 +1,38 @@
 import React, { useState } from "react";
 
+
 function Form() {
   const [firstName, setFirstName] = useState("John");
   const [lastName, setLastName] = useState("Henry");
 
-  return (
-    <form>
-      <input type="text" value={firstName} />
-      <input type="text" value={lastName} />
-      <button type="submit">Submit</button>
-    </form>
-  );
-}
 
+  function Form(props) {
+    return (
+      <form>
+        <input type="text" value={firstName} />
+        <input type="text" value={lastName} />
+        <input
+          type="text"
+          onChange={props.handleFirstNameChange}
+          value={props.firstName}
+        />
+        <input
+          type="text"
+          onChange={props.handleLastNameChange}
+          value={props.lastName}
+        />
+        <input
+          type="checkbox"
+          id="newsletter"
+          onChange={props.handleNewsletterChange}
+          /* checked instead of value */
+          checked={props.newsletter}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    );
+  }
+}  
 export default Form;
+
+
